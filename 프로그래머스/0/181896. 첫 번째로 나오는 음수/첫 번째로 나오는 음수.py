4b17a07e82db3cdf9b81f680cda276a6d63597c4
yhsys0429-create <yhsys0429@gmail.com>
2026-07-18 15:18:18 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -2,6 +2,6 @@ def solution(num_list):
     answer = -1
     for i in range(len(num_list)):
         if num_list[i] < 0:
-            answer = i
+            answer = i       
             break
     return answer
