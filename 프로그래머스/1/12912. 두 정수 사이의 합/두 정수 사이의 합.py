810dbd722f6e7bda06b7d7a9aeb56a3c08ecfdc7
yhsys0429-create <yhsys0429@gmail.com>
2026-07-18 15:44:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 497.73 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,11 +1,12 @@
 def solution(a, b):
+    if a > b:
+        temp = a
+        a = b
+        b = temp
     if a == b:
         return a or b
     answer = 0
     if a < b:
         for i in range(a,b+1):
-            answer += i
-    else:
-        for i in range(b,a+1):
             answer += i
     return answer        
